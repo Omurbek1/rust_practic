78a2e9e4ebd002c2438296a89ec72549313c2af7
Omur <omurbekmamytbekov545@gmail.com>
2024-03-27 18:54:50 +0600

    use import modules

--- lesson10_import/src/main.rs
+++ lesson10_import/src/main.rs
@@ -1,3 +1,29 @@
+
+pub mod salam{
+    pub mod adamAttar{
+        pub mod turkish{
+            pub fn hello(){
+                println!("Merhaba");
+            }
+        }
+        pub mod english{
+            pub fn hello(){
+                println!("Hello");
+            }
+        }
+        pub mod spanish{
+            pub fn hello(){
+                println!("Hola");
+            }
+        }
+        pub mod kyrgyz{
+            pub fn hello(){
+                println!("Salam");
+            }
+        }
+    }
+}
+use salam::adamAttar::kyrgyz;
 fn main() {
-    println!("Hello, world!");
+    kyrgyz::hello();
 }
