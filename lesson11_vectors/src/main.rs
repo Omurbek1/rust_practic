d83661fb9c625eee2b0e7ff875f0e4da08afd8c0
Omur <omurbekmamytbekov545@gmail.com>
2024-03-27 19:17:41 +0600

    vectors

--- lesson11_vectors/src/main.rs
+++ lesson11_vectors/src/main.rs
@@ -3,6 +3,25 @@ fn main() {
     let v2=vec![0;10];
     let v3=vec![1,2,3,4,5];
     let v4=&v3[1..4];
-    println!("{:?}",v4);
+    
+    println!("{:?}",v4,);
+    println!("{:?}",v2);
     println!("Hello, world!");
+    vectors();
+}
+
+fn vectors(){
+    #[derive(Debug)]
+    enum SpreadSheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
+
+    let row=vec![
+        SpreadSheetCell::Int(3),
+        SpreadSheetCell::Text(String::from("blue")),
+        SpreadSheetCell::Float(10.12),
+    ];
+    println!("{:?}",row);
 }
